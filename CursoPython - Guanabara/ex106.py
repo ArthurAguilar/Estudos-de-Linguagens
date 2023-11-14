4c5944662c35e2ec80f07eb379e047c9be9da4ee
ArthurAguilar <arthurg.programming@gmail.com>
2023-11-14 16:02:05 -0300

    Estudos

--- CursoPython - Guanabara/ex106.py
+++ CursoPython - Guanabara/ex106.py
@@ -1,3 +1,4 @@
+from time import sleep
 cor = {
     'padrao': '\033[m',
     'vermelho': '\033[0;30;41m',
@@ -6,8 +7,25 @@ cor = {
     'azul': '\033[0;30;44m',
     'rosa': '\033[0;30;45m',
     'branco': '\033[0;30m'
-}
+    }
 
 
-print(f'{cor["verde"]} Olá, galeres!{cor["padrao"]}')
-print(f'{cor["roxo"]}Tudo bem??{cor["padrao"]}')
+def titulo(msg):
+    """Título do programa, com cores e linhas
+
+    Args:
+        msg (_type_):Defina qual será o título
+    """
+    linha = '*'*len(msg)
+    print(f'{cor["verde"]}{linha}\n{msg}\n{linha}{cor["padrao"]}')
+
+
+def manual():
+    titulo('Systema de ajuda PyCharm')
+    fb = str(input('Função ou biblioteca: '))
+    titulo(f'Acessando o manual do comando "{fb}".')
+    sleep(1)
+    help(fb)
+
+
+manual()
