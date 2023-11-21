6e73584d2fc2e5c18fdfb11a32a5e6cdb8888612
ArthurAguilar <arthurg.programming@gmail.com>
2023-11-20 23:15:13 -0300

    Estudos

--- CursoPython - Guanabara/ex101+/ex115.py
+++ CursoPython - Guanabara/ex101+/ex115.py
@@ -1,4 +1,31 @@
 from uteis.projetoCeV import *
 
+while True:
+   
+    option = menuPrincipal(['Pessoas Cadastradas', 'Cadastrar Novas Pessoas',
+               'Sair do Sistema'])
 
-menuPrincipal()
+    if option == 1:
+        linha()
+        print(f'[bold blue]PESSOAS CADASTRADAS'.center(50))
+        linha()
+        print()
+        linha()
+        sleep(2)
+    elif option == 2:
+        linha()
+        print(f'[bold blue]CADASTRO'.center(50))
+        linha()
+        print()
+        linha()
+        sleep(2)
+    elif option == 3:
+        print('[red]\nSAINDO DO PROGRAMA', end='')
+        for i in range(3):
+            sleep(.5)
+            print('[red].', end='') 
+        print('\n[red]FALOOOOU!')  
+        break             
+    else:
+        print(f'[red]Valor inválido, tente entre do número 1 ao 3!')
+
--- CursoPython - Guanabara/ex101+/uteis/projetoCeV.py
+++ CursoPython - Guanabara/ex101+/uteis/projetoCeV.py
@@ -1,43 +1,29 @@
 from rich import print
 from time import sleep
+from uteis.dados import leiaInt
 
 def linha():
     print('-'*40)
 
 
-def menuPrincipal():
+def title(msg):
     linha()
-    print('[bold blue]MENU PRINCIPAL'.center(50))
+    print(msg)
     linha()
-    print('''[yellow][1] - [blue]Ver pessoas cadastradas
-[yellow][2] - [blue]Cadastrar novas pessoas
-[yellow][3] - [blue]Sair do sistema''')
-    linha()
-    
+
+
+def menuPrincipal(lista):
+    title('[bold blue]MENU PRINCIPAL'.center(50))
+    c = 1
+    for item in lista:
+        print(f'[yellow]{c} - [blue]{item}')
+        c += 1
+    linha()    
     while True:
-        print('[bold blue]Digite sua opção: ', end='')
-        try: 
-            option = int(input())
-            if option == 1:
-                linha()
-                print(f'[bold blue]PESSOAS CADASTRADAS'.center(50))
-                linha()
-                print()
-                linha()
-            elif option == 2:
-                linha()
-                print(f'[bold blue]CADASTRO'.center(50))
-                linha()
-                print()
-                linha()
-            elif option == 3:
-                print('[red]\nSAINDO DO PROGRAMA', end='')
-                for i in range(3):
-                    sleep(.5)
-                    print('[red].', end='') 
-                print('\n[red]FALOOOOU!')               
-                break
-            else:
-                print('[red]Valor inválido, tente entre do número 1 ao 3!')
-        except:
-                print(f'[red]Valor inválido, tente novamente!')
+        try:
+            print('[bold blue]Digite sua opção: ', end='')
+            n = int(input())
+            return n
+        except (ValueError, TypeError):
+            print(f'[red]ERROR! Não é um número inteiro, tente novamente.')            
+
