8a8f2ae85fd9bc46d826e200a8ed5a833388063e
Arthur Aguilar <arthurg.programming@gmail.com>
2024-01-22 16:01:03 -0300

    aprendendo while em JS + HTML-CSS

---
+++ JavaScript (Guanabara)/while-condicoes/ex008.js
@@ -0,0 +1,8 @@
+let vel = 90
+
+console.log(`Sua velocidade foi de ${vel}km/h.`)
+if (vel > 80) {
+    console.log('Você foi MULTADO por ultrapassar o limite de velocidade')
+}
+console.log('Dirija com cuidado. Use sempre cinto de segurança!')
+
