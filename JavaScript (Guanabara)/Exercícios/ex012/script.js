96bf6750f306e14c4dce943b471142c17acf9c8b
Arthur Aguilar <arthurg.programming@gmail.com>
2024-01-23 21:34:46 -0300

    exercicio-completo

--- JavaScript (Guanabara)/Exercícios/ex012/script.js
+++ JavaScript (Guanabara)/Exercícios/ex012/script.js
@@ -1,10 +1,11 @@
 function horaAtual() {
     let hora = new Date().getHours()
+    let min = new Date().getMinutes()
     let txt = document.querySelector('#horaAgora') 
-    let img = document.querySelector('#img')
+    let img = document.querySelector('#imagemAgora')
     
     hora = 19
-    txt.innerHTML = `Agora são exatamente ${hora}!`
+    txt.innerHTML = `Agora são exatamente ${hora}:${min} horas!`
     if (hora < 12) {
         document.body.style.background = '#fcc575'
         img.src = 'foto-manha.png'
@@ -13,6 +14,10 @@ function horaAtual() {
         img.src = 'foto-tarde.png'
     } else {
         document.body.style.background = '#212a27'
+        document.querySelector('#titulo').style.color ='white'
+        document.querySelector('footer').style.color = 'white'
+
+       
         img.src = 'foto-noite.png'
     }
 }
