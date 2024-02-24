f7cdf45d5b490083cb8d08c514e48f7ac117e99b
Arthur Aguilar <arthurg.programming@gmail.com>
2024-02-24 09:32:29 -0300

    + Estudos

--- JavaScript - CBF/ex021/contatos.js
+++ JavaScript - CBF/ex021/contatos.js
@@ -3,24 +3,36 @@
 import { agenda } from "./bancoContatos.js"
 
 let contatos = {
-    addContato: (dados, destino) => {
-        const div = document.createElement('div')
-        div.classList.add('contato')
+    addContato: (dados, destino) => {    
+        
+        const novoContato = {
+            nome: dados.nome,
+            tel: dados.tel,
+            email: dados.email
+        }
 
-        const pNome = document.createElement('p')
-        pNome.innerHTML = dados.nome
+        agenda.push(novoContato)
+        
+        agenda.forEach((contato) => {
+            const div = document.createElement('div')
+            div.classList.add('contato')
+    
+            const pNome = document.createElement('p')
+            pNome.innerHTML = contato.nome
+    
+            const pTel = document.createElement('p')
+            pTel.innerHTML = contato.tel
+    
+            const pEmail = document.createElement('p')
+            pEmail.innerHTML = contato.email
+    
+            div.appendChild(pNome)
+            div.appendChild(pTel)
+            div.appendChild(pEmail)
+            destino.appendChild(div)
+        })
 
-        const pTel = document.createElement('p')
-        pTel.innerHTML = dados.tel
-
-        const pEmail = document.createElement('p')
-        pEmail.innerHTML = dados.email
-
-        div.appendChild(pNome)
-        div.appendChild(pTel)
-        div.appendChild(pEmail)
-
-        destino.appendChild(div)
+        console.log(agenda)
     }
 }
 
