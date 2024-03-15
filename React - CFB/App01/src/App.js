cb4fd6217128b148a25263aeff7dc85e9cb171f5
Arthur Aguilar <arthurg.programming@gmail.com>
2024-03-15 19:16:31 -0300

    + Estudos

--- React - CFB/App01/src/App.js
+++ React - CFB/App01/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Meu primeiro app de React.JS! :D
-        </a>
-      </header>
-    </div>
-  );
+      <div className='containerPai'>
+        <p>Hello World! Olá mundo!</p>
+        <p>Oi, eu sou o Arthur Aguilar!</p>
+        <p>Sou desenvolvedor de software.</p>
+        <p>Hoje estou aprendendo sobre ReactJS</p>
+      </div>
+  )
 }
-
-export default App;
--- React - CFB/teste.js
+++
@@ -1,6 +0,0 @@
-const pessoa = [1, 2, 3, 4]
-
-
-const pessoaCopia = pessoa.map(cadaNumero => cadaNumero + 1)
-
-console.log(pessoaCopia)
