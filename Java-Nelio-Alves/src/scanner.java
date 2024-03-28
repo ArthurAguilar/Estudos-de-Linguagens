f1ca675f1d59b38b9dcb0a7916d0e5ddcd2703bb
Arthur Aguilar <arthurg.programming@gmail.com>
2024-03-27 22:48:35 -0300

    Testando novamente os commits no IntellJ

--- Java-Nelio-Alves/src/scanner.java
+++ Java-Nelio-Alves/src/scanner.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class scanner {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
         String x;
+        int y;
 
+        System.out.print("Digite o seu nome: ");
         x = sc.nextLine();
-        System.out.println("Você digitou: " + x);
+
+        System.out.print("Digite a sua idade: ");
+        y = sc.nextInt();
         sc.close();
+
+        System.out.printf("Seu nome é %s e você tem %d anos. Prazer!", x, y);
     }
 }
