a3548933dc3bb39cee927e3726af6e707bad925b
Arthur Aguilar <arthurg.programming@gmail.com>
2024-03-28 23:16:11 -0300

    Aprendendo a usar IntelliJ

--- Java-Nelio-Alves/src/Main.java
+++ Java-Nelio-Alves/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        String nome = "Arthur";
+        int idade = 25;
 
+        System.out.printf("Meu nome é %s e tenho %d anos.", nome, idade);
+        
     }
 }
--- Java-Nelio-Alves/src/scanner.java
+++ Java-Nelio-Alves/src/scanner.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class scanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String x;
-        int y;
 
         System.out.print("Digite o seu nome: ");
-        x = sc.nextLine();
+        String x = sc.next();
 
-        System.out.print("Digite a sua idade: ");
-        y = sc.nextInt();
+        System.out.print("Quantos anos você tem? ");
+        int y = sc.nextInt();
+
+        System.out.print("Qual o seu salário? ");
+        double z = sc.nextDouble();
+
+        System.out.printf("Seu nome é %s, você tem %d e seu salário atual é R$%.2f", x, y, z);
+
         sc.close();
-
-        System.out.printf("Seu nome é %s e você tem %d anos. Prazer!", x, y);
     }
 }
