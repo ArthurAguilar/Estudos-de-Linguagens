577d93493a2f93ebd38c830a93fe81fdc85ab217
Arthur Aguilar <arthurg.programming@gmail.com>
2024-03-29 23:02:54 -0300

    + Estudos!

--- Java-Nelio-Alves/src/EstruturasRepetitivas/exercises/ex03.java
+++ Java-Nelio-Alves/src/EstruturasRepetitivas/exercises/ex03.java
@@ -1,4 +1,31 @@
 package EstruturasRepetitivas.exercises;
+import java.util.Scanner;
 public class ex03 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite o tamanho do loop: ");
+        int n = sc.nextInt();
+
+        int in = 0;
+        int out = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um número: ");
+            int x = sc.nextInt();
+
+            if (x < 10 || x > 20) {
+                out += 1;
+            } else {
+                in += 1;
+            }
+        }
+        
+        System.out.printf("%d in", in);
+        System.out.printf("%d out", out);
+
+        sc.close();
+    }
 
 }
---
+++ Java-Nelio-Alves/src/Funções/functions.java
@@ -0,0 +1,27 @@
+package Funções;
+import java.util.Scanner;
+
+public class functions {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter three numbers: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        int higher = max(x, y, z);
+        showResults(higher);
+        
+        sc.close();
+    }
+
+    public static int max(int a, int b, int c) {
+        return (a > b && a > c) ? a : (b > c) ? b : c;
+    }
+
+    public static void showResults(int higherValue) {
+        System.out.print("O maior valor é: " + higherValue);
+    }
+}
