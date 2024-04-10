057e3508ef7d717f18ad503c0e47b570b10af0fa
Arthur Aguilar <arthurg.programming@gmail.com>
2024-04-10 15:31:24 -0300

    + Estudos

---
+++ Java-Nelio-Alves/src/index.js
@@ -0,0 +1,16 @@
+console.log("Escolha uma opção abaixo: ")
+
+switch (opcao) {
+    case 1:
+        console.log("Check-in realizado com sucesso.")
+        break
+    case 2:
+        console.log("Check-out realizado com sucesso.")
+        break
+    case 3:
+        console.log("Encerrando atendimento.")
+        break
+    default:
+        console.log("opcao não encontrada")
+        break
+}
