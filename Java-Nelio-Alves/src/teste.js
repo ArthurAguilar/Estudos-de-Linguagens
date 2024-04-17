1fc43dbb84193e6613f95826d51199d5cfdd7572
Arthur Aguilar <arthurg.programming@gmail.com>
2024-04-16 21:13:36 -0300

    xD

--- Java-Nelio-Alves/src/teste.js
+++ Java-Nelio-Alves/src/teste.js
@@ -1,9 +1,15 @@
-let names = [
+names = [
     'Arthur', 
     'Amanda',
     'Renner', 
     'Gaby'
-]
+];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i])
+}
+
+console.log('---------------------')
 
 for (nome of names) {
     console.log(nome)
