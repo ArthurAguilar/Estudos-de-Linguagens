22963450b6f772474da2eb02bcd97505e5516f57
Arthur Aguilar <arthurg.programming@gmail.com>
2024-04-17 21:59:53 -0300

    testes

--- Java-Nelio-Alves/src/teste.js
+++ Java-Nelio-Alves/src/teste.js
@@ -1,7 +1,10 @@
-public class Main {
-    public statid void main (String [] args) {
-        int x = 5;
-        int resultado = x++ * x++;
-        system.out.printIn(resultado)
-    }
+nomes = [
+    'Arthur', 
+    'Amanda',
+    'Renner',
+    'Gaby'
+]
+
+for (nome of nomes) {
+    console.log(nome)
 }
