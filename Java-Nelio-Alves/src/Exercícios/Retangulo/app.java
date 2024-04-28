a9ea312e6380752fa23f78a42a26296ef5490f80
Arthur Aguilar <arthurg.programming@gmail.com>
2024-04-28 16:15:37 -0300

    + Estudos

--- Java-Nelio-Alves/src/Exercícios/Retangulo/app.java
+++ Java-Nelio-Alves/src/Exercícios/Retangulo/app.java
@@ -1,5 +1,23 @@
 package Exercícios.Retangulo;
 
-public class app {
+import java.util.Scanner;
 
+import Exercícios.Retangulo.entities.Retangulo;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Retangulo retangulo = new Retangulo();
+        
+        System.out.println("Digite a altura e a largura do triângulo: ");
+        retangulo.altura = sc.nextDouble();
+        retangulo.largura = sc.nextDouble();
+
+        System.out.printf("Área: %.2f\n", retangulo.calcularArea());
+        System.out.printf("Perímetro: %.2f\n", retangulo.calcularPerimetro());
+        System.out.printf("Diagonal: %.2f\n", retangulo.calcularDiagonal());
+        
+        sc.close();
+    }
 }
