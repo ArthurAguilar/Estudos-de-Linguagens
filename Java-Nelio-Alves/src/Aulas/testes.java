f68843ce334404b7becff8b4841616a53e31a94e
Arthur Aguilar <arthurg.programming@gmail.com>
2024-04-30 20:37:35 -0300

    Fix: org

--- Java-Nelio-Alves/src/Aulas/testes.java
+++ Java-Nelio-Alves/src/Aulas/testes.java
@@ -1,8 +1,16 @@
 package Aulas;
 public class testes {
     public static void main(String[] args) {
-        int x = 5 ;
-        int result = x > 2 ? x < 4 ? 10 : 8 : 7;
-        System.out.println(result);
+
+        int[] numeros = {10, 20, 30, 40, 50};
+        int[] numbers = {100, 200, 300, 400, 500};
+
+        for (int nums : numeros) {
+            System.out.print(nums+ " ");
+        }
+        System.out.println();
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
     }
 }
