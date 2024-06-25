416985dad5c97b82c5ae324c6c59fd22cf3fb4ff
Arthur Aguilar <arthurg.programming@gmail.com>
2024-06-24 21:41:12 -0300

    Exercícios Curso + configurando IntelliJ

---
+++ Java-Nelio-Alves/src/Exercícios/ArraysVetores/ProblemasExemplos/ProblemaExemploUm.java
@@ -0,0 +1,11 @@
+package ArraysVetores.ProblemasExemplos;
+
+import java.util.Scanner;
+
+public class ProblemaExemploUm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
