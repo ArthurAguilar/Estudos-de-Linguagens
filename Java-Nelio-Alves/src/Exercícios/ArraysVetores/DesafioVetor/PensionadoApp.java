e0cd0b62195ce47d47976a002e7f78311449dbee
Arthur Aguilar <arthurg.programming@gmail.com>
2024-07-02 20:40:44 -0300

    testing

--- Java-Nelio-Alves/src/Exercícios/ArraysVetores/DesafioVetor/PensionadoApp.java
+++ Java-Nelio-Alves/src/Exercícios/ArraysVetores/DesafioVetor/PensionadoApp.java
@@ -15,7 +15,7 @@ public class PensionadoApp {
         Pensionado[] aluguelQuartos = Pensionado.alugarQuarto(sc, numEstudantes);
         
         Pensionado.mostrarRelatorio(aluguelQuartos);
- 
+
         sc.close();
     }
 }
