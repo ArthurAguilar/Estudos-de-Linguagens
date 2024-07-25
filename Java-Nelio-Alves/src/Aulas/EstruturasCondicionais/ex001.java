3ddb8cc5cbc2bbe9a97bc35bb30c6b8ae3710fce
Arthur Aguilar <arthurg.programming@gmail.com>
2024-07-24 23:00:41 -0300

    xd

--- Java-Nelio-Alves/src/Aulas/EstruturasCondicionais/ex001.java
+++ Java-Nelio-Alves/src/Aulas/EstruturasCondicionais/ex001.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class ex001 {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite um número: ");
