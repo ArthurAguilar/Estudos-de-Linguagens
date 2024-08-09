1f2462d5a6a7def09b0e3beef45f2c26ae94ff34
Arthur Aguilar <arthurg.programming@gmail.com>
2024-08-08 23:31:04 -0300

    Exercising

--- Java-Nelio-Alves/src/testingForEach.java
+++ Java-Nelio-Alves/src/testingForEach.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class testingForEach {
     public static void main(String[] args) {
                 
-        int[] numbers = {12, 23, 34, 45, 57};
+        List<Integer> nums = new ArrayList<>();
 
-        int index = 0;
-        for (int number : numbers) {
-            System.out.printf("Índice: %d Número: %d%n", index, number);;
-            index++;
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        nums.forEach(num -> {
+            System.out.println(num);
+        });
+
+        for (int i = 0; i < nums.size(); i++) {
+            int numero = nums.get(i);
+            System.out.println("Índice: " + i + ", Valor: " + numero);
         }
     }
 }
