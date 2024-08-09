182e832a537d14a0dd2d3df19a74383f2035a60c
Arthur Aguilar <arthurg.programming@gmail.com>
2024-08-09 00:04:39 -0300

    fixings format

--- Java-Nelio-Alves/src/Aulas/UpDowncasting/Program.java
+++ Java-Nelio-Alves/src/Aulas/UpDowncasting/Program.java
@@ -6,7 +6,7 @@ import Aulas.UpDowncasting.entities.Account;
 
 public class Program {
     public static void main(String[] args) {
-        
+
         Account acc = new Account(10, "Tuuzano", 1500);
         BusinessAccount bacc = new BusinessAccount(11, "Mandioca", 3000, 5000);
 
@@ -15,10 +15,10 @@ public class Program {
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(12, "Renner", 4000, 10000);
         Account acc3 = new SavingAccounts(13, "Gaby", 6000, .5);
-                
+
         // DOWNCASTING
 
-        BusinessAccount bacc1 = (BusinessAccount)acc2;
+        BusinessAccount bacc1 = (BusinessAccount) acc2;
         SavingAccounts sacc = new SavingAccounts(14, "Sofhia", 12000, .5);
 
         if (acc instanceof Account) {
@@ -26,7 +26,7 @@ public class Program {
         } else {
             System.out.println("Não é uma Account!");
         }
-        
+
         if (bacc1 instanceof BusinessAccount) {
             System.out.println("É uma instancia de Business Account!");
         } else {
