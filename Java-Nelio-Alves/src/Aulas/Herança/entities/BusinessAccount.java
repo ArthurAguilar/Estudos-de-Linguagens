d8353434e0ec52844756719d0733a26abd08db6b
Arthur Aguilar <arthurg.programming@gmail.com>
2024-08-08 23:09:48 -0300

    Learning about 'herança'

---
+++ Java-Nelio-Alves/src/Aulas/Herança/entities/BusinessAccount.java
@@ -0,0 +1,19 @@
+package Aulas.Herança.entities;
+
+public class BusinessAccount extends Account {
+
+    private double loanLimit;
+
+    public BusinessAccount(){}
+
+    public BusinessAccount(Integer number, String holder, double balance, double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }    
+
+    public void loan (double amount) {
+        if (amount <= loanLimit) {
+            deposit(amount);
+        }
+    }
+}
