8568c3724b2d61f20fd24121761d2e094026b6c5
Arthur Aguilar <arthurg.programming@gmail.com>
2024-08-09 22:54:34 -0300

    tests

--- Java-Nelio-Alves/src/testingForEach.java
+++ Java-Nelio-Alves/src/testingForEach.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class testingForEach {
     public static void main(String[] args) {
                 
-        List<Integer> nums = new ArrayList<>();
+        List<Integer> nums = new ArrayList<>(Arrays.asList(11, 22, 33, 44, 55));
 
         nums.add(1);
         nums.add(2);
